c4d7aaac2b3ed768ff9910e06349746c1e24c158
jhavikash01 <vikashkumarjha9199@gmail.com>
2025-12-26 23:02:26 +0530

    Create login.jsx

---
+++ login.jsx
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div className="login">
+      <h2>Sign In</h2>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
